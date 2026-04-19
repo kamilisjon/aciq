4b722252432fc68e2f62c64c24ee49dbe0671f63
kamilisjon <kamilis.jonk@gmail.com>
2026-04-19 14:04:03 +0300

    remove indirection

--- aciq/models/resnet.py
+++ aciq/models/resnet.py
@@ -3,23 +3,21 @@ import tinygrad.nn as nn
 from tinygrad import Tensor, dtypes
 from tinygrad.helpers import fetch, get_child
 
-# allow monkeypatching in layer implementations
-BatchNorm = nn.BatchNorm2d
-Conv2d = nn.Conv2d
-Linear = nn.Linear
-
 
 class BasicBlock:
   expansion = 1
 
   def __init__(self, in_planes, planes, stride=1):
-    self.conv1 = Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-    self.bn1 = BatchNorm(planes)
-    self.conv2 = Conv2d(planes, planes, kernel_size=3, padding=1, stride=1, bias=False)
-    self.bn2 = BatchNorm(planes)
+    self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+    self.bn1 = nn.BatchNorm2d(planes)
+    self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, padding=1, stride=1, bias=False)
+    self.bn2 = nn.BatchNorm2d(planes)
     self.downsample = []
     if stride != 1 or in_planes != self.expansion * planes:
-      self.downsample = [Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False), BatchNorm(self.expansion * planes)]
+      self.downsample = [
+        nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+        nn.BatchNorm2d(self.expansion * planes),
+      ]
 
   def __call__(self, x):
     out = self.bn1(self.conv1(x)).relu()
@@ -33,15 +31,18 @@ class Bottleneck:
   expansion = 4
 
   def __init__(self, in_planes, planes, stride=1):
-    self.conv1 = Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=False)
-    self.bn1 = BatchNorm(planes)
-    self.conv2 = Conv2d(planes, planes, kernel_size=3, padding=1, stride=1, bias=False)
-    self.bn2 = BatchNorm(planes)
-    self.conv3 = Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
-    self.bn3 = BatchNorm(self.expansion * planes)
+    self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=False)
+    self.bn1 = nn.BatchNorm2d(planes)
+    self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, padding=1, stride=1, bias=False)
+    self.bn2 = nn.BatchNorm2d(planes)
+    self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+    self.bn3 = nn.BatchNorm2d(self.expansion * planes)
     self.downsample = []
     if stride != 1 or in_planes != self.expansion * planes:
-      self.downsample = [Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False), BatchNorm(self.expansion * planes)]
+      self.downsample = [
+        nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+        nn.BatchNorm2d(self.expansion * planes),
+      ]
 
   def __call__(self, x):
     out = self.bn1(self.conv1(x)).relu()
@@ -62,13 +63,13 @@ class ResNet:
 
     self.in_planes = 64
 
-    self.conv1 = Conv2d(3, 64, kernel_size=7, stride=2, bias=False, padding=3)
-    self.bn1 = BatchNorm(64)
+    self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, bias=False, padding=3)
+    self.bn1 = nn.BatchNorm2d(64)
     self.layer1 = self._make_layer(self.block, 64, self.num_blocks[0], stride=1)
     self.layer2 = self._make_layer(self.block, 128, self.num_blocks[1], stride=2)
     self.layer3 = self._make_layer(self.block, 256, self.num_blocks[2], stride=2)
     self.layer4 = self._make_layer(self.block, 512, self.num_blocks[3], stride=2)
-    self.fc = Linear(512 * self.block.expansion, num_classes)
+    self.fc = nn.Linear(512 * self.block.expansion, num_classes)
 
   def _make_layer(self, block, planes, num_blocks, stride):
     strides = [stride] + [1] * (num_blocks - 1)
