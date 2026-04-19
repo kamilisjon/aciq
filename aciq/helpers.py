a60a5c92c5b9c120de4670034388313c966c45be
kamilisjon <kamilis.jonk@gmail.com>
2026-04-19 10:37:46 +0300

    format

--- aciq/helpers.py
+++ aciq/helpers.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 PATH_DATETIME_FORMAT = "%Y%m%d_%H%M%S"
 
+
 def get_output_dir(root: Path, prefix: str = "") -> Path:
   timestamp = datetime.now().strftime(PATH_DATETIME_FORMAT)
   return root / (f"{prefix}_{timestamp}" if prefix else timestamp)
